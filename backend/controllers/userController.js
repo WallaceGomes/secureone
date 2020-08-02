584bf98fd80a7bc13d75d537a5caedc984443072
WallaceGomes <wallacecardosogomes@gmail.com>
2020-08-02 15:06:20 -0300

    chore: added userController methods and model configs

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -6,7 +6,7 @@ const HttpError = require('../models/http-error');
 exports.index = async (req, res, next) => {
 	let users;
 	try {
-		users = await user.find({}, '-password');
+		users = await user.find({ role: 'client' }, '-password');
 	} catch (err) {
 		console.error(err);
 		const error = new HttpError('Could not find any user!', 500);
@@ -17,7 +17,18 @@ exports.index = async (req, res, next) => {
 };
 
 exports.create = async (req, res, next) => {
-	const { name, email, password } = req.body;
+	const {
+		name,
+		email,
+		password,
+		role = 'client',
+		confirmed = false,
+		address,
+		cnpj,
+		status,
+		contract,
+		due_date,
+	} = req.body;
 
 	try {
 		const checkUser = await User.findOne({ email: email });
@@ -47,6 +58,13 @@ exports.create = async (req, res, next) => {
 		name,
 		email,
 		password: hashedPassword,
+		confirmed,
+		role,
+		address,
+		cnpj,
+		status,
+		contract,
+		due_date,
 	});
 
 	try {
@@ -94,22 +112,135 @@ exports.login = async (req, res, next) => {
 		return next(error);
 	}
 
-	let token;
+	if (user.confirmed) {
+		let token;
+		try {
+			token = jwt.sign(
+				{ userId: user.id, email: user.email },
+				process.env.JWT_KEY,
+				{ expiresIn: '12h' },
+			);
+		} catch (err) {
+			const error = new HttpError('Login failed, JWT', 500);
+			return next(error);
+		}
+
+		return res.status(201).json({
+			userName: user.name,
+			role: user.role,
+			userId: user.id,
+			email: user.email,
+			token: token,
+		});
+	} else {
+		return res.status(200).send(false);
+	}
+};
+
+exports.changePassword = async (req, res, next) => {
+	const { email, password } = req.body;
+
+	let user;
 	try {
-		token = jwt.sign(
-			{ userId: user.id, email: user.email },
-			process.env.JWT_KEY,
-			{ expiresIn: '12h' },
-		);
+		user = await User.findOne({ email: email });
 	} catch (err) {
-		const error = new HttpError('Login failed, JWT', 500);
+		const error = new HttpError('Unable to find the user', 500);
 		return next(error);
 	}
 
-	return res.status(201).json({
-		userName: user.name,
-		userId: user.id,
-		email: user.email,
-		token: token,
-	});
+	let hashedPassword;
+	try {
+		hashedPassword = await bcrypt.hash(password, 12);
+	} catch (err) {
+		console.error(err);
+		const error = new HttpError(
+			'Could not create user, try again later.(hash error)',
+			500,
+		);
+		return next(error);
+	}
+
+	const updatedUser = {
+		password: hashedPassword,
+		updated_at: Date.now(),
+		confirmed: true,
+	};
+
+	try {
+		await User.findOneAndUpdate({ email: email }, updatedUser);
+	} catch (err) {
+		const error = new HttpError('Unexpected error', 500);
+		console.log(err);
+		return next(error);
+	}
+
+	res.status(200).json({ message: 'Your password has been changed!' });
+};
+
+exports.update = async (req, res, next) => {
+	const { userId } = req.params;
+	const { email, name, address, cnpj, status, contract, due_date } = req.body;
+
+	const updatedUser = {
+		email: email,
+		name: name,
+		address: address,
+		cnpj: cnpj,
+		status: status,
+		contract: contract,
+		due_date: due_date,
+		updated_at: Date.now(),
+	};
+
+	let user;
+
+	try {
+		user = await User.findOneAndUpdate({ _id: userId }, updatedUser);
+	} catch (err) {
+		const error = new HttpError('User already exists or DB error', 500);
+		return next(error);
+	}
+
+	if (!user) {
+		const error = new HttpError(
+			'Could not find any user for the provided ID',
+			404,
+		);
+		return next(error);
+	}
+
+	res.status(200).json({ message: 'User updated!' });
+};
+
+exports.delete = async (req, res, next) => {
+	const { userId } = req.params;
+
+	let user;
+
+	try {
+		user = await User.findById({ _id: userId });
+	} catch (err) {
+		const error = new HttpError('Error to find the user', 500);
+		return next(error);
+	}
+
+	if (!user) {
+		const error = new HttpError(
+			'Could not find any user for the provided id',
+			404,
+		);
+		return next(error);
+	}
+
+	try {
+		const sess = await mongoose.startSession();
+		sess.startTransaction();
+		await user.remove({ session: sess });
+		await sess.commitTransaction();
+	} catch (err) {
+		const error = new HttpError('Error deleting the user, DB session', 500);
+		return next(error);
+	}
+
+	res.status(200).json({ message: 'User deleted!' });
 };
--- backend/models/user.js
+++ backend/models/user.js
@@ -7,6 +7,13 @@ const userSchema = new Schema({
 	name: { type: String, required: true },
 	email: { type: String, required: true, unique: true },
 	password: { type: String, required: true },
+	confirmed: { type: Boolean, required: true },
+	role: { type: String },
+	address: { type: String },
+	cnpj: { type: String },
+	status: { type: Boolean },
+	contract: { type: String },
+	due_date: { type: Date },
 	created_at: { type: Date },
 	updated_at: { type: Date },
 });
