e55a00a514ee02a25c2093eb003d394ff3045315
WallaceGomes <wallacecardosogomes@gmail.com>
2020-08-15 22:45:37 -0300

    chore(feat): added proper form handling with formik and yup to admin form

--- web/src/pages/Admin/index.js
+++ web/src/pages/Admin/index.js
@@ -7,18 +7,22 @@ import {
 	Header,
 	ClientsTable,
 	Card,
+	Error,
+	Label,
 } from './styles';
 import Menu from '../../components/Menu';
 import LoadingSpinner from '../../components/LoadingSpinner';
 import { useHttpClient } from '../../hooks/http-hook';
-import Input from '../../components/Input';
+
+import { Formik, Field, Form, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
 import Button from '../../components/Button';
-import Select from '../../components/Select';
 
 const Admin = () => {
 	const { sendRequest, isLoading } = useHttpClient();
 	const [users, setUsers] = useState([]);
 
+	const [initialFormValues, setInitialFormValues] = useState();
 	const [editClientName, setEditClientName] = useState('');
 	const [editClientEmail, setEditClientEmail] = useState('');
 	const [editClientAddress, setEditClientAddress] = useState('');
@@ -39,89 +43,32 @@ const Admin = () => {
 		);
 	}, [sendRequest]);
 
-	/**
-
-	const now = new Date(user.due_date);
-
-			const day = ('0' + now.getDate()).slice(-2);
-			const month = ('0' + (now.getMonth() + 1)).slice(-2);
-
-			const today = now.getFullYear() + '-' + month + '-' + day;
-
-			console.log(day);
-	 */
-
 	const switchEditModeHandler = (user) => {
 		if (user) {
-			setEditClientAddress(user.address);
-			setEditClientCNPJ(user.cnpj);
-			setEditClientEmail(user.email);
-			setEditClientName(user.name);
-			setEditClientID(user._id);
-			setEditClientActive(user.status);
-			setEditClientContract(user.contract);
-			setEditClientDueDate(user.due_date);
+			const initialvalues = {
+				name: user.name,
+				email: user.email,
+				address: user.address,
+				cnpj: user.cnpj,
+				contract: user.contract,
+				status: user.status,
+				due_date: user.due_date,
+				clientId: user._id,
+			};
+			setInitialFormValues(initialvalues);
 		}
 		setIsEditMode((prevMode) => !prevMode);
 	};
 
-	const editUserSubmitHandler = async (event) => {
-		event.preventDefault();
-		try {
-			//vai precisar disso aqui depois
-			/**
-			 * {
-					'Content-Type': 'application/json',
-					Authorization: `Bearer ${storedData.token}`,
-				},
-			 */
-			await sendRequest(
-				`http://localhost:3333/api/users/clients/${editClietID}`,
-				'PATCH',
-				JSON.stringify({
-					name: editClientName,
-					email: editClientEmail,
-					address: editClientAddress,
-					cnpj: editClientCNPJ,
-					contract: editClientContract,
-					status: editClientActive,
-					due_date: editClientDueDate,
-				}),
-				{
-					'Content-Type': 'application/json',
-				},
-			);
-			const editedUser = {
-				_id: editClietID,
-				name: editClientName,
-				email: editClientEmail,
-				address: editClientAddress,
-				cnpj: editClientCNPJ,
-				contract: editClientContract,
-				due_date: editClientDueDate,
-				status: editClientActive,
-			};
-			const index = users.findIndex((user) => user._id === editClietID);
-			if (index !== -1) {
-				const auxUsers = [...users];
-				auxUsers[index] = editedUser;
-				setUsers(auxUsers);
-			}
-		} catch (err) {
-			console.log(err);
-		}
-		switchEditModeHandler();
-	};
-
-	const deleteClientHandler = async (editClietID, event) => {
+	const deleteClientHandler = async (clientId, event) => {
 		event.preventDefault();
 		try {
 			const response = await sendRequest(
-				`http://localhost:3333/api/users/clients/${editClietID}`,
+				`http://localhost:3333/api/users/clients/${clientId}`,
 				'DELETE',
 				null,
 			);
-			const index = users.findIndex((user) => user._id === editClietID);
+			const index = users.findIndex((user) => user._id === clientId);
 			if (index !== -1) {
 				const newUsers = [...users];
 				newUsers.splice(index, 1);
@@ -139,61 +86,131 @@ const Admin = () => {
 			<Container>
 				<Menu />
 				<Card>
-					<form onSubmit={editUserSubmitHandler}>
-						<h1>Editar Cliente</h1>
-						<Input
-							name="editClientName"
-							type="text"
-							value={editClientName}
-							placeholder="Nome"
-							setValue={setEditClientName}
-						/>
-						<Input
-							name="editClientEmail"
-							type="email"
-							value={editClientEmail}
-							placeholder="E-mail"
-							setValue={setEditClientEmail}
-						/>
-						<Input
-							name="editClientAddress"
-							type="text"
-							value={editClientAddress}
-							placeholder="Endereço"
-							setValue={setEditClientAddress}
-						/>
-						<Input
-							name="editClientCNPJ"
-							type="text"
-							value={editClientCNPJ}
-							placeholder="CNPJ"
-							setValue={setEditClientCNPJ}
-						/>
-						<Select
-							name="editClientActive"
-							type="select"
-							value={editClientActive}
-							setValue={setEditClientActive}
-						/>
-						<Input
-							name="editClientContract"
-							type="text"
-							value={editClientContract}
-							placeholder="Contrato"
-							setValue={setEditClientContract}
-						/>
-						<Input
-							name="editClientDueDate"
-							type="date"
-							value={editClientDueDate}
-							placeholder="Data de Vencimento do Contrato"
-							setValue={setEditClientDueDate}
-						/>
-						<div>
-							<Button type="submit"> Editar </Button>
+					<Formik
+						initialValues={{
+							name: initialFormValues.name,
+							email: initialFormValues.email,
+							address: initialFormValues.address,
+							cnpj: initialFormValues.cnpj,
+							contract: initialFormValues.cnpj,
+							due_date: initialFormValues.due_date,
+							status: initialFormValues.status,
+							clientId: initialFormValues.clientId,
+						}}
+						validationSchema={Yup.object({
+							name: Yup.string().required('Campo obrigatório'),
+							email: Yup.string()
+								.email('Formato de email inválido')
+								.required('Campo obrigatório'),
+							address: Yup.string().required('Campo obrigatório'),
+							cnpj: Yup.string().required('Campo obrigatório'),
+							contract: Yup.string().required('Campo obrigatório'),
+							due_date: Yup.string().required('Campo obrigatório'),
+							status: Yup.string().required('Campo obrigatório'),
+						})}
+						onSubmit={async (values, { setSubmitting }) => {
+							try {
+								//vai precisar disso aqui depois
+								/**
+								 * {
+										'Content-Type': 'application/json',
+										Authorization: `Bearer ${storedData.token}`,
+									},
+								 */
+								await sendRequest(
+									`http://localhost:3333/api/users/clients/${values.clientId}`,
+									'PATCH',
+									JSON.stringify({
+										name: values.name,
+										email: values.email,
+										address: values.address,
+										cnpj: values.cnpj,
+										contract: values.contract,
+										due_date: values.due_date,
+										status: values.status,
+									}),
+									{
+										'Content-Type': 'application/json',
+									},
+								);
+								const editedUser = {
+									_id: values.clientId,
+									name: values.name,
+									email: values.email,
+									address: values.address,
+									cnpj: values.cnpj,
+									contract: values.contract,
+									due_date: values.due_date,
+									status: values.status,
+								};
+								const index = users.findIndex(
+									(user) => user._id === values.clientId,
+								);
+								if (index !== -1) {
+									const auxUsers = [...users];
+									auxUsers[index] = editedUser;
+									setUsers(auxUsers);
+								}
+								setSubmitting(false);
+							} catch (err) {
+								console.log(err);
+							}
+							switchEditModeHandler();
+						}}
+					>
+						<Form>
+							<div>
+								<h1>Editar cliente</h1>
+
+								<Label htmlFor="name">Nome </Label>
+								<Field name="name" type="text" />
+								<Error>
+									<ErrorMessage name="name" />
+								</Error>
+
+								<Label htmlFor="email">Email</Label>
+								<Field name="email" type="email" />
+								<Error>
+									<ErrorMessage name="email" />
+								</Error>
+
+								<Label htmlFor="address">Endereço</Label>
+								<Field name="address" type="text" />
+								<Error>
+									<ErrorMessage name="address" />
+								</Error>
+
+								<Label htmlFor="cnpj">CNPJ</Label>
+								<Field name="cnpj" type="text" />
+								<Error>
+									<ErrorMessage name="cnpj" />
+								</Error>
+
+								<Label htmlFor="status">Status</Label>
+								<Field name="status" as="select">
+									<option value="Ativo">Ativo</option>
+									<option value="Inativo">Inativo</option>
+								</Field>
+								<Error>
+									<ErrorMessage name="status" />
+								</Error>
+
+								<Label htmlFor="contract">Contrato</Label>
+								<Field name="contract" type="text" />
+								<Error>
+									<ErrorMessage name="contract" />
+								</Error>
+
+								<Label htmlFor="due_date">Vencimento do contrato</Label>
+								<Field name="due_date" type="date" />
+								<Error>
+									<ErrorMessage name="due_date" />
+								</Error>
+							</div>
+							<Button type="submit">Editar</Button>
 							<Button onClick={switchEditModeHandler}> Voltar </Button>
-						</div>
-					</form>
+						</Form>
+					</Formik>
 				</Card>
 			</Container>
 		);
--- web/src/pages/Admin/styles.js
+++ web/src/pages/Admin/styles.js
@@ -51,28 +51,86 @@ export const ClientsTable = styled.table`
 export const Card = styled.div`
 	width: 35%;
 	margin-left: 12%;
-	margin-top: 50px;
-	height: 90vh;
+	margin-top: 20px;
+	height: 95vh;
 	position: relative;
 	box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
 	border-radius: 6px;
-	padding: 1rem;
 	overflow: hidden;
 	background: white;
 
 	h1 {
-		margin: 2rem;
+		margin-left: 2rem;
+		margin-top: 1rem;
+		margin-bottom: 1rem;
 	}
 
-	Select Input {
+	input,
+	select {
 		width: 90%;
 		margin-left: 2rem;
 		margin-right: 2rem;
+		margin-top: 5px;
+		background-color: #fff;
+		border: 2px solid #808080;
+		border-radius: 3px;
+		padding: 8px;
+		font-size: 16px;
+		transition: 180ms ease-in-out;
+
+		&:focus {
+			border: 2px solid #333333;
+		}
 	}
 
 	Button {
 		width: 40%;
 		height: 40px;
+		font-size: 18px;
+		margin-top: 2rem;
 		margin-left: 2rem;
 	}
 `;
+
+export const Error = styled.div`
+	color: red;
+	margin-left: 2rem;
+	font-weight: bold;
+	margin-top: 5px;
+	margin-bottom: 15px;
+`;
+
+export const Label = styled.label`
+	font-weight: bold;
+	margin-left: 2rem;
+	font-size: 20px;
+`;
+
+// export const Card = styled.div`
+// 	width: 35%;
+// 	margin-left: 12%;
+// 	margin-top: 50px;
+// 	height: 90vh;
+// 	position: relative;
+// 	box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+// 	border-radius: 6px;
+// 	padding: 1rem;
+// 	overflow: hidden;
+// 	background: white;
+
+// 	h1 {
+// 		margin: 2rem;
+// 	}
+
+// 	Select Input {
+// 		width: 90%;
+// 		margin-left: 2rem;
+// 		margin-right: 2rem;
+// 	}
+
+// 	Button {
+// 		width: 40%;
+// 		height: 40px;
+// 		margin-left: 2rem;
+// 	}
+// `;
