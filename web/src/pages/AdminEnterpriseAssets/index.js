b5dfe1f55bb9fe86cdaffa2bf5ce367fd6385e9d
WallaceGomes <wallacecardosogomes@gmail.com>
2020-08-25 22:37:26 -0300

    chore(pages): added button to add assets from admin page

--- web/src/pages/AdminEnterpriseAssets/index.js
+++ web/src/pages/AdminEnterpriseAssets/index.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { AiFillEdit, AiFillDelete } from 'react-icons/ai';
+import Modal from 'react-modal';
 
 import {
 	Container,
@@ -19,21 +20,55 @@ import { Formik, Field, Form, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
 import Button from '../../components/Button';
 
+//Modal styles
+const customStyles = {
+	overlay: {
+		position: 'fixed',
+		top: 0,
+		left: 0,
+		right: 0,
+		bottom: 0,
+		backgroundColor: 'rgba(0, 0, 0, 0.75)',
+	},
+	content: {
+		top: '50%',
+		left: '50%',
+		right: 'auto',
+		bottom: 'auto',
+		transform: 'translate(-50%, -50%)',
+		backgroundColor: '#fff',
+		color: '#000',
+		fontWeight: 'bold',
+	},
+};
+
+Modal.setAppElement('#root');
+
 const AdminEnterpriseAssets = () => {
 	const { sendRequest, isLoading } = useHttpClient();
 
 	const [assets, setAssets] = useState([]);
-	// const [clients, setClients] = useState([]);
+	const [clients, setClients] = useState([]);
 	const [initialFormValues, setInitialFormValues] = useState();
 	const [isEditMode, setIsEditMode] = useState(false);
+	const [isNewAssetMode, setIsNewAssetMode] = useState(false);
+
+	const [modalIsOpen, setIsOpen] = useState(false);
+	const [modalText, setModalText] = useState('');
+
+	function switchModalState() {
+		setIsOpen((prevMode) => !prevMode);
+	}
 
 	//falta auth
 	useEffect(() => {
-		// sendRequest('https://secureone-backend.herokuapp.com/api/users/clients', 'GET', null).then(
-		// 	(response) => {
-		// 		setClients(response);
-		// 	},
-		// );
+		//https://secureone-backend.herokuapp.com
+		//http://localhost:3333
+		sendRequest('http://localhost:3333/api/users/clients', 'GET', null).then(
+			(response) => {
+				setClients(response);
+			},
+		);
 
 		//https://secureone-backend.herokuapp.com
 		//http://localhost:3333
@@ -43,6 +78,10 @@ const AdminEnterpriseAssets = () => {
 			},
 		);
 	}, [sendRequest]);
+
+	const switchNewAssetMode = () => {
+		setIsNewAssetMode((prevMode) => !prevMode);
+	};
 
 	const switchEditModeHandler = (asset) => {
 		if (asset) {
@@ -88,6 +127,268 @@ const AdminEnterpriseAssets = () => {
 			alert(err.message);
 		}
 	};
+
+	if (isNewAssetMode) {
+		return (
+			<Container>
+				<Menu />
+				{isLoading && <LoadingSpinner asOverlay />}
+				<Card>
+					<Formik
+						initialValues={{
+							equipment: '',
+							modelo: '',
+							hostname: '',
+							user: '',
+							memory: '',
+							cpu: '',
+							hd: '',
+							so: '',
+							licensed: '',
+							antivirus: '',
+							tdr: '',
+							inuse: '',
+							clientId: '',
+						}}
+						validationSchema={Yup.object({
+							clientId: Yup.string().required('Selecione o cliente'),
+							equipment: Yup.string().required('Campo obrigatório'),
+							modelo: Yup.string().required('Campo obrigatório'),
+							hostname: Yup.string().required('Campo obrigatório'),
+							user: Yup.string().required('Campo obrigatório'),
+							memory: Yup.string().required('Campo obrigatório'),
+							cpu: Yup.string().required('Campo obrigatório'),
+							hd: Yup.string().required('Campo obrigatório'),
+							so: Yup.string().required('Campo obrigatório'),
+							licensed: Yup.string().required('Campo obrigatório'),
+							antivirus: Yup.string().required('Campo obrigatório'),
+							tdr: Yup.string().required('Campo obrigatório'),
+							inuse: Yup.string().required('Campo obrigatório'),
+						})}
+						onSubmit={async (values, { setSubmitting }) => {
+							console.log(values);
+							//https://secureone-backend.herokuapp.com
+							//http://localhost:3333
+							try {
+								const response = await sendRequest(
+									'http://localhost:3333/api/client/assets',
+									'POST',
+									JSON.stringify({
+										clientId: values.clientId,
+										equipment: values.equipment,
+										modelo: values.modelo,
+										hostname: values.hostname,
+										user: values.user,
+										memory: values.memory,
+										cpu: values.cpu,
+										hd: values.hd,
+										so: values.so,
+										licensed: values.licensed,
+										antivirus: values.antivirus,
+										tdr: values.tdr,
+										inuse: values.inuse,
+									}),
+									{
+										'Content-Type': 'application/json',
+									},
+								);
+								const newAsset = {
+									_id: response._id,
+									equipment: response.equipment,
+									modelo: response.modelo,
+									hostname: response.hostname,
+									user: response.user,
+									memory: response.memory,
+									cpu: response.cpu,
+									hd: response.hd,
+									so: response.so,
+									licensed: response.licensed,
+									antivirus: response.antivirus,
+									tdr: response.tdr,
+									inuse: response.inuse,
+								};
+
+								const ausAssets = [...assets];
+								ausAssets.push(newAsset);
+								setAssets(ausAssets);
+
+								setSubmitting(false);
+								setModalText('Nova ativo criado com sucesso!');
+								switchModalState();
+								switchNewAssetMode();
+							} catch (err) {
+								console.error(err);
+							}
+						}}
+					>
+						<Form>
+							<div>
+								<h1>Cadastrar novo ativo</h1>
+
+								<DuoColumns>
+									<div>
+										<Label htmlFor="clientId">Nome do cliente</Label>
+										<Field
+											name="clientId"
+											as="select"
+											placeholder="Selecione o cliente"
+										>
+											<option defaultChecked>Selecione o cliente</option>
+											{clients.map((client) => {
+												return (
+													<option key={client._id} value={client._id}>
+														{client.name}
+													</option>
+												);
+											})}
+										</Field>
+										<Error>
+											<ErrorMessage name="clientId" />
+										</Error>
+									</div>
+
+									<div>
+										<Label htmlFor="equipment">Equipamento</Label>
+										<Field name="equipment" as="select">
+											<option defaultChecked>Selecione</option>
+											<option value="Notebook">Notebook</option>
+											<option value="Desktop">Desktop</option>
+										</Field>
+										<Error>
+											<ErrorMessage name="equipment" />
+										</Error>
+									</div>
+								</DuoColumns>
+
+								<Label htmlFor="modelo">Modelo</Label>
+								<Field name="modelo" type="text" />
+								<Error>
+									<ErrorMessage name="modelo" />
+								</Error>
+
+								<Label htmlFor="hostname">Host Name</Label>
+								<Field name="hostname" type="text" />
+								<Error>
+									<ErrorMessage name="hostname" />
+								</Error>
+
+								<Label htmlFor="user">Usuário</Label>
+								<Field name="user" type="text" />
+								<Error>
+									<ErrorMessage name="user" />
+								</Error>
+
+								<DuoColumns>
+									<div>
+										<Label htmlFor="cpu">Processador</Label>
+										<Field name="cpu" type="text" />
+										<Error>
+											<ErrorMessage name="cpu" />
+										</Error>
+									</div>
+
+									<div>
+										<Label htmlFor="hd">Disco</Label>
+										<Field name="hd" type="text" />
+										<Error>
+											<ErrorMessage name="hd" />
+										</Error>
+									</div>
+								</DuoColumns>
+
+								<DuoColumns>
+									<div>
+										<Label htmlFor="memory">Memória</Label>
+										<Field name="memory" as="select">
+											<option defaultChecked>Selecione</option>
+											<option value="8GB">8GB</option>
+											<option value="12GB">12GB</option>
+											<option value="16GB">16GB</option>
+											<option value="32GB">32GB</option>
+											<option value="64GB">64GB</option>
+											<option value="128GB">128GB</option>
+											<option value="256BB">256BB</option>
+											<option value="Outros">Outros</option>
+										</Field>
+										<Error>
+											<ErrorMessage name="memory" />
+										</Error>
+									</div>
+
+									<div>
+										<Label htmlFor="so">Sistema Operacional</Label>
+										<Field name="so" as="select">
+											<option defaultChecked>Selecione</option>
+											<option value="Windows 10 Pro">Windows 10 Pro</option>
+											<option value="Linux Ubunto">Linux Ubunto</option>
+											<option value="Outros">Outros</option>
+										</Field>
+										<Error>
+											<ErrorMessage name="so" />
+										</Error>
+									</div>
+								</DuoColumns>
+
+								<DuoColumns>
+									<div>
+										<Label htmlFor="licensed">Licenciado</Label>
+										<Field name="licensed" as="select">
+											<option defaultChecked>Selecione</option>
+											<option value="Sim">Sim</option>
+											<option value="Não">Não</option>
+										</Field>
+										<Error>
+											<ErrorMessage name="licensed" />
+										</Error>
+									</div>
+
+									<div>
+										<Label htmlFor="antivirus">Antivírus</Label>
+										<Field name="antivirus" as="select">
+											<option defaultChecked>Selecione</option>
+											<option value="Sim">Sim</option>
+											<option value="Não">Não</option>
+										</Field>
+										<Error>
+											<ErrorMessage name="antivirus" />
+										</Error>
+									</div>
+								</DuoColumns>
+
+								<DuoColumns>
+									<div>
+										<Label htmlFor="tdr">TDR Watchguard</Label>
+										<Field name="tdr" as="select">
+											<option defaultChecked>Selecione</option>
+											<option value="Sim">Sim</option>
+											<option value="Não">Não</option>
+										</Field>
+										<Error>
+											<ErrorMessage name="tdr" />
+										</Error>
+									</div>
+
+									<div>
+										<Label htmlFor="inuse">Em uso</Label>
+										<Field name="inuse" as="select">
+											<option defaultChecked>Selecione</option>
+											<option value="Sim">Sim</option>
+											<option value="Não">Não</option>
+										</Field>
+										<Error>
+											<ErrorMessage name="inuse" />
+										</Error>
+									</div>
+								</DuoColumns>
+							</div>
+							<Button type="submit">Registrar ativo</Button>
+							<Button onClick={switchNewAssetMode}> Voltar </Button>
+						</Form>
+					</Formik>
+				</Card>
+			</Container>
+		);
+	}
 
 	if (isEditMode) {
 		return (
@@ -180,10 +481,12 @@ const AdminEnterpriseAssets = () => {
 									setAssets(auxAssets);
 								}
 								setSubmitting(false);
+								setModalText('Ativo editado com sucesso!');
+								switchModalState();
+								switchEditModeHandler();
 							} catch (err) {
 								console.log(err);
 							}
-							switchEditModeHandler();
 						}}
 					>
 						<Form>
@@ -330,8 +633,20 @@ const AdminEnterpriseAssets = () => {
 			<UsersContainer>
 				<Header>
 					<h1>Lista de ativos cadastrados</h1>
+					<Button onClick={switchNewAssetMode}>Adicionar Novo Ativo</Button>
 				</Header>
 				{isLoading && <LoadingSpinner />}
+				<Modal
+					isOpen={modalIsOpen}
+					onRequestClose={switchModalState}
+					style={customStyles}
+					contentLabel="Alert Modal"
+				>
+					<div>{modalText}</div>
+					<br />
+					<br />
+					<Button onClick={switchModalState}>OK</Button>
+				</Modal>
 				<ClientsTable>
 					<tbody>
 						<tr>
